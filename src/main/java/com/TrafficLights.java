8738ea22b01d74febd017eabede1345622905884
feyerina <feyerina@mail.ru>
2018-07-08 22:21:03 +0300

    update structure

--- src/main/java/com/TrafficLights.java
+++ src/main/java/com/TrafficLights.java
@@ -1,8 +1,10 @@
+package com;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class TrafficLights {
+
     public static final String MINUTE = "минут";
     public static final String SECOND = "секунд";
     public static void main(String[] args) {
